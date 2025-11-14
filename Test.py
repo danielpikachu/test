beda007149e5e65d981cc996a97f062ca7a5cafd
Pikachu <daniel_han2008@hotmail.com>
2025-11-14 10:45:02 +0800

    Update Test.py

--- Test.py
+++ Test.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import streamlit as st
-from google.cloud import datastore
+
 
 # 初始化Google Cloud Datastore客户端
 datastore_client = datastore.Client()
@@ -950,3 +950,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
