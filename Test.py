868bdde028037e26fc8d42005080a1adf567927e
Pikachu <daniel_han2008@hotmail.com>
2025-11-16 16:10:36 +0800

    Update Test.py

--- Test.py
+++ Test.py
@@ -827,7 +827,7 @@ def welcome_page():
             align-items: center;
             justify-content: center;
             padding-top: 20px; 
-            margin-top: -120px;
+            margin-top: -200px;
             padding: 0;
             text-align: center;
         }
@@ -1041,6 +1041,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
