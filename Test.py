f622deab27a8a32371ba525d22e7ca2ccecb2012
Pikachu <daniel_han2008@hotmail.com>
2026-03-25 13:02:44 +0800

    Update Test.py

--- Test.py
+++ Test.py
@@ -1050,7 +1050,7 @@ def main():
                 st.session_state['page'] = 'welcome'
                 st.rerun()
 
-        st.markdown('<h3 style="margin:0.5rem 0; line-height:1;font-size:20px;">🏫 SCIS Campus 3D Navigation System</h4>', unsafe_allow_html=True)
+        st.markdown('<h3 style="margin:0.5rem 0; line-height:1.2;">🏫 SCIS Campus 3D Navigation System</h4>', unsafe_allow_html=True)
         
         school_data = load_school_data_detailed('school_data_detailed.json')
         if school_data is None:
