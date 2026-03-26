152e433f63f19a949871727712d4f563d8d0616c
Pikachu <daniel_han2008@hotmail.com>
2026-03-26 11:51:43 +0800

    Update Test.py

--- Test.py
+++ Test.py
@@ -911,7 +911,7 @@ div.block-container {
     padding-bottom: 0rem !important;
 }
 ::-webkit-scrollbar {display: none !important;}
-section[data-testid="stSidebar"] > div:first-child {padding-top: -0.2rem !important;}
+section[data-testid="stSidebar"] > div:first-child {padding-top: -0.4rem !important;}
 </style>
 """, unsafe_allow_html=True)
 
