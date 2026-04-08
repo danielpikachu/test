16c4d4da069a94c6de75447c11d66f3963387b72
Pikachu <daniel_han2008@hotmail.com>
2026-04-08 13:17:58 +0800

    Update Test.py

--- Test.py
+++ Test.py
@@ -1077,7 +1077,7 @@ def main():
         else:
             fig = plot_3d_map(school_data, graph)[0]
 
-        st.markdown("<div style='height:12px'></div>", unsafe_allow_html=True)
+        st.markdown("<div style='height:20px'></div>", unsafe_allow_html=True)
 
         st.plotly_chart(
             fig,
